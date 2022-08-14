4ce836e274c96876bf135e332db4253da66fcd1b
ivanalchuk <altschukiwan@gmail.com>
2022-08-14 16:27:59 +0300

    Fix test

--- src/test/java/com/alchk/awsec2/ControllerTest.java
+++ src/test/java/com/alchk/awsec2/ControllerTest.java
@@ -38,6 +38,6 @@ public class ControllerTest {
         Mockito.when(this.dummyJpaRepository.findAll()).thenReturn(List.of());
 
         this.mockMvc.perform(get("/main/dummy"))
-                .andExpect(status().isCreated());
+                .andExpect(status().isOk());
     }
 }
