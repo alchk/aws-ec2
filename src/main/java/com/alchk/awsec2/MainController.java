b8c18af3924ae060e27ded3e6a66a2878a5dd5b8
ivanalchuk <altschukiwan@gmail.com>
2022-09-15 00:10:04 +0300

    Add jar

--- src/main/java/com/alchk/awsec2/MainController.java
+++ src/main/java/com/alchk/awsec2/MainController.java
@@ -1,6 +1,7 @@
 package com.alchk.awsec2;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,13 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "main")
 @RequiredArgsConstructor
+@Slf4j
 public class MainController {
 
 
     @GetMapping(value = "/dummy")
     public ResponseEntity<List<String>> getAllDummies() {
+        log.info("Request proceeded");
         return ResponseEntity.ok(List.of("Hi", "Hello", "Lol"));
     }
 }
